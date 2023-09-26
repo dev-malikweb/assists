4e5c995a3b170873cc17c6955a98ec5b398fe4f8
dev-malikweb <145810098+dev-malikweb@users.noreply.github.com>
2023-09-26 11:46:48 +0300

    Update auto-matches-table-change-imgs.js

--- auto-matches-table-change-imgs.js
+++ auto-matches-table-change-imgs.js
@@ -1,5 +1,5 @@
 const imageReplacements = {
-//فرق الدوري الإنجليزي
+// الأندية الإنجليزية 
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/09/burnley.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/12.png',//بيرنلي
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/udQ6ns69PctCv143h-GeYw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/105.png',//مانشستر يونايتد
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/z44l-a0W1v5FmgPnemV6Xw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/110.png',//مانشستر سيتي
@@ -14,18 +14,21 @@ const imageReplacements = {
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/download-1.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/114.png',//توتنهام
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/02/sheffield.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/20.png',//شيفيلد يونايتد
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/96CcNNQ0AYDAbssP0V9LuQ_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/116.png',//نيوكاسل يونايتد
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/UDYY4FSlty6fXFBzvFfcyw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/22.png',//ليستر سيتي
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/8piQOzndGmApKYTcvyN9vA_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/10.png',//كريستال بالاس
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/09/EKIe0e-ZIphOcfQAwsuEEQ_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/38.png',//برايتون
 
-
-// فرق الدوري الأسباني
+// الأندية الإسبانية
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/pk-hNCNpGM_JDoHHvLVF-Q_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/143.png',//اوساسونا
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/hCTs5EX3WjCMC5Jl3QE4Rw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/135.png',//اشبيلية
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/download-3.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/132.png',//برشلونة
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/wpdhixHP_sloegfP0UlwAw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/158.png',//سيلتا فيجو
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/Th4fAVAZeCJWRcKoLW7koA_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/131.png',//ريال مدريد
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/09/srAAE0bOnCppUrlbJpFiHQ_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/134.png',//اتلتيكو مدريد
+'https://stad.yalla-shoots.io/wp-content/uploads/2022/11/ud-almeria.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/138.png',//الميريا
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/Ss21P4CUmigjrEtcoapjVg_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/142.png',//ريال مايوركا
 
-
-// فرق الدوري الإيطالي
+// الأندية الإيطالية
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/09/GoeTFIVAZLA5JWk0-A6B0A_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/266.png',//ساسولو
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/Lv6xmBlUIpN3GAFhtf6nqQ_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/226.png',//يوفنتوس
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/VoKsJ6RitaHGhsM62e6AXQ_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/227.png',//ميلان
@@ -36,35 +39,41 @@ const imageReplacements = {
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/PWRLYBJqlGrAAsKkUN6eng_96x96-1.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/234.png',//نابولي
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/09/ovE3HSEx4GWXkW8GU7KVhA_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/235.png',//تورينو
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/BQdP4jUBFJfG7U_JBsFIMg_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/225.png',//روما
+'https://stad.yalla-shoots.io/wp-content/uploads/2022/10/lecce.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/246.png',//ليتشي
 
-
-// فرق الدوري الفرنسي
+// الأندية الفرنسية
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/08/paris-sg.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/480.png',//باريس سان جيرمان
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/KfBX1kHNj26r9NxpqNaTkA_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/469.png',//مارسيليا
 
-
-// فرق الدوري الألماني
+// الأندية الألمانية
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/cmntP5q_pHL7g5LfkRiw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/331.png',//بايرن ميونخ
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/09/بوخوم.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/343.png',//بوخوم
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/FZnTSH2rbHFos4BnlWAItw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/341.png',//دورتموند
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/07/nCdwmPlnAA041M4HDMzmkg_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/339.png',//فولفسبورج
+'https://stad.yalla-shoots.io/wp-content/uploads/2023/09/preusen-munster.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/423.png',//بروسيا مونستر
 
-
-// فرق الدوري المصري
+// الأندية المصرية
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/09/al-ahly.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/8200.png',//الأهلي
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/06/RDTXHVLGhO3Emt27QslOCw_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/8201.png',//الزمالك
+'https://stad.yalla-shoots.io/wp-content/uploads/2021/06/Ra_Fuh-xdYTK41_UUigPVA_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/8310.png',//المقاولون العرب
 
-
-// فرق الدوري السعودي
+// الأندية السعودية
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/01/al-okhdood.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/55794.png',//الاخدود
 'https://stad.yalla-shoots.io/wp-content/uploads/2022/10/al-khaleej.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/12138.png',//الخليج
 'https://stad.yalla-shoots.io/wp-content/uploads/2023/01/al-nasr.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/7549.png',//النصر
 'https://stad.yalla-shoots.io/wp-content/uploads/2021/08/8S9rNUFk1YO27XsLV6jn_Q_96x96.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/8946.png',//الاهلي السعودي
-
+'https://stad.yalla-shoots.io/wp-content/uploads/2023/02/uhud.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/14903.png',//أحد
+'https://stad.yalla-shoots.io/wp-content/uploads/2023/02/al-kholood.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/55800.png',//الخلود
 
 // فرق دوري أبطال أوروبا
 
 
 // فرق دوري أبطال أفريقيا
+
+
+// الفرف الامريكية
+'https://stad.yalla-shoots.io/wp-content/uploads/2023/06/inter-miami.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/54729.png',//إنتر ميامي
+'https://stad.yalla-shoots.io/wp-content/uploads/2023/08/orlando-city.png': 'https://cdn.statically.io/gh/dev-malikweb/images/teams-imgs/12204.png',//أورلاندو سيتي
 
 
 };
